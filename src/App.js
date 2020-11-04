1d941506ca87af4215be3c390a654c4de9cc1a7e
Stéphanie D <spdumont89@gmail.com>
2020-11-04 23:16:57 +0100

    quete2.2 props

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import React from 'react';
-import Contact from './components/Contact'
-
+import React from "react";
+import Contact from "./components/Contact";
 
 function App() {
   return (
     <div className="App">
-      <Contact />
+      <Contact
+        name="Esther Wood"
+        img="https://randomuser.me/api/portraits/women/32.jpg"
+        offline
+      />
+      <Contact
+        name="Isobel Steeves"
+        img="https://randomuser.me/api/portraits/women/72.jpg"
+        online
+      />
+      <Contact
+        name="Bobby Wilson"
+        img="https://randomuser.me/api/portraits/men/79.jpg"
+        online
+      />
     </div>
   );
 }
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,23 +1,52 @@
 import React from "react";
 import "./Contact.css";
+import PropTypes from "prop-types";
 
-const online = true;
-
-function Contact() {
+// Quete 2.2
+function Contact(props) {
     return (
         <figure className="Contact">
-            <img
-                className="avatar"
-                src="https://randomuser.me/api/portraits/women/32.jpg"
-                alt="contact"
-            />
+            <img className="avatar" src={props.img} alt={props.name} />
+
             <div className="status">
-                <p className="name">Esther Wood</p>
-                <div className={online ? "status-online" : "status-offline"}></div>
-                <div className="status-text">{online ? "Online" : "Offline"}</div>
+                <p className="name">{props.name}</p>
+                <div
+                    className={props.online ? "status-online" : "status-offline"}
+                ></div>
+                <div className="status-text">{props.online ? "Online" : "Offline"}</div>
             </div>
         </figure>
     );
 }
 
+Contact.propTypes = {
+    name: PropTypes.string.isRequired,
+    img: PropTypes.object.isRequired,
+    online: PropTypes.bool.isRequired
+};
+
 export default Contact;
+
+// Quete 2.1
+// const online = true;
+
+// function Contact() {
+//     return (
+//         <figure className="Contact">
+//             <img
+//                 className="avatar"
+//                 src="https://randomuser.me/api/portraits/women/32.jpg"
+//                 alt="contact"
+//             />
+//             <div className="status">
+//                 <p className="name">Esther Wood</p>
+//                 <div className={online ? "status-online" : "status-offline"}></div>
+//                 <div className="status-text">{online ? "Online" : "Offline"}</div>
+//             </div>
+//         </figure>
+//     );
+// }
+
+
+
+
