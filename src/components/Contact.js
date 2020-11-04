e24ac48950c1b1d2b1a5e0e0c8e5874e7c559009
Stéphanie D <spdumont89@gmail.com>
2020-11-04 18:29:41 +0100

    modi div status

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,20 +1,20 @@
-import React from 'react';
-import './Contact.css';
+import React from "react";
+import "./Contact.css";
 
 const online = true;
 
 function Contact() {
     return (
         <figure className="Contact">
-            <img className="avatar"
+            <img
+                className="avatar"
                 src="https://randomuser.me/api/portraits/women/32.jpg"
                 alt="contact"
             />
             <div className="status">
                 <p className="name">Esther Wood</p>
-                <div className={online ? 'status-online' : 'status-offline'}>
-                    <div className="status-text">{online ? "Online" : 'Offline'}</div>
-                </div>
+                <div className={online ? "status-online" : "status-offline"}></div>
+                <div className="status-text">{online ? "Online" : "Offline"}</div>
             </div>
         </figure>
     );
